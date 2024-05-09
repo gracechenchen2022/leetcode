412c1917d8fe4381429547c6f9e175ca95e8c4ad
gracechenchen2022 <112357023+gracechenchen2022@users.noreply.github.com>
2024-05-09 00:03:25 -0400

    Runtime: 82 ms (88.25%), Memory: 21.1 MB (56.43%) - Gitcode

---
+++ 347-top-k-frequent-elements/347. Top K Frequent Elements.py
@@ -0,0 +1,10 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums)
+        heap = []
+        for key, val in count.items():
+            heapq.heappush(heap,(val, key))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        top_k = [key for val, key in heap]
+        return top_k
