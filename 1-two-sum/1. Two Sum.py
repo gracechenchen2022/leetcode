338018d69e3082c15e438744fe880612365c53e7
gracechenchen2022 <112357023+gracechenchen2022@users.noreply.github.com>
2024-05-27 14:24:52 -0400

    Runtime: 57 ms (73.07%), Memory: 17.6 MB (56.02%) - Gitcode

--- 1-two-sum/1. Two Sum.py
+++ 1-two-sum/1. Two Sum.py
@@ -4,7 +4,7 @@ class Solution:
         for i, n in enumerate(nums):
             diff = target - n
             if diff in preMap:
-                return[preMap[diff],i]
+                return(preMap[diff],i)
             preMap[n] = i
 # i , n  0, 2; 1, 7; 2, 11; 3, 15;
 # target = 9  diff = 9-2 preMap[2] = 0
