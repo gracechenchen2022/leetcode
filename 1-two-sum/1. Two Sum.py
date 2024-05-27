c257a724d209b4090aa8a36eb7b47c6b1a9bfe64
gracechenchen2022 <112357023+gracechenchen2022@users.noreply.github.com>
2024-05-27 14:13:47 -0400

    Runtime: 59 ms (65.09%), Memory: 17.7 MB (40.92%) - Gitcode

--- 1-two-sum/1. Two Sum.py
+++ 1-two-sum/1. Two Sum.py
@@ -1,9 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         preMap = {}
-        for i, n, in enumerate(nums):
+        for i, n in enumerate(nums):
             diff = target - n
             if diff in preMap:
-                return[preMap[diff], i]
+                return[preMap[diff],i]
             preMap[n] = i
-        return
+# i , n  0, 2; 1, 7; 2, 11; 3, 15;
+# target = 9  diff = 9-2 preMap[2] = 0
+# diff = 9-7 return  0,1 preMap[]
