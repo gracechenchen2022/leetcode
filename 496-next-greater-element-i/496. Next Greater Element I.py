d2169f9510c1e9b5a9d44cd12a17d51b07cb8285
gracechenchen2022 <112357023+gracechenchen2022@users.noreply.github.com>
2024-06-14 16:09:29 -0400

    Runtime: 41 ms (91.06%), Memory: 16.8 MB (75.78%) - Gitcode

--- 496-next-greater-element-i/496. Next Greater Element I.py
+++ 496-next-greater-element-i/496. Next Greater Element I.py
@@ -3,10 +3,11 @@ class Solution:
         next_greater = {}
         stack = []
         for num in nums2:
-            while stack and stack[-1]< num:
+            while stack and stack[-1] < num:
                 next_greater[stack.pop()] = num
             stack.append(num)
         while stack:
             next_greater[stack.pop()] = -1
         res = [next_greater[num] for num in nums1]
+
         return res
