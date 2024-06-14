2e330b661cc159e4ae47d7b9531d2ac7bc2ee0b1
gracechenchen2022 <112357023+gracechenchen2022@users.noreply.github.com>
2024-06-14 14:06:16 -0400

    Runtime: 36 ms (44.56%), Memory: 16.4 MB (72.77%) - Gitcode

---
+++ 246-strobogrammatic-number/246. Strobogrammatic Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isStrobogrammatic(self, num: str) -> bool:
+        pairs = {'0':'0','1':'1','6':'9','8':'8','9':'6'}
+        left, right = 0, len(num) -1
+        while left <= right:
+            if num[left] not in pairs or num[right] not in pairs:
+                return False
+            if pairs[num[left]] != num[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
