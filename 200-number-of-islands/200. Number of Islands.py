edec3ac36e712753fbacb4fcbf4220e58086aaa0
gracechenchen2022 <112357023+gracechenchen2022@users.noreply.github.com>
2024-07-06 00:24:06 -0400

    Runtime: 249 ms (49.96%), Memory: 18.8 MB (89.79%) - Gitcode

---
+++ 200-number-of-islands/200. Number of Islands.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+
+
+        if not grid:
+            return 0
+
+        nr = len(grid)
+        nc = len(grid[0])
+        num_islands = 0
+
+        def bfs(grid, r, c):
+            queue = deque([(r, c)])
+            while queue:
+                row, col = queue.popleft()
+                for x, y in [(row-1, col), (row+1, col), (row, col-1), (row, col+1)]:
+                    if 0 <= x < nr and 0 <= y < nc and grid[x][y] == '1':
+                        grid[x][y] = '0'
+                        queue.append((x, y))
+
+        for r in range(nr):
+            for c in range(nc):
+                if grid[r][c] == '1':
+                    num_islands += 1
+                    bfs(grid, r, c)
+
+        return num_islands
