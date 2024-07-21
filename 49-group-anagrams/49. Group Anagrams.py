45993e8888c74ed4f18e934445ed3137d60a7c13
gracechenchen2022 <112357023+gracechenchen2022@users.noreply.github.com>
2024-07-21 00:17:05 -0400

    Runtime: 85 ms (77.95%), Memory: 20.5 MB (41.70%) - Gitcode

--- 49-group-anagrams/49. Group Anagrams.py
+++ 49-group-anagrams/49. Group Anagrams.py
@@ -1,8 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         ans = defaultdict(list)
-
         for s in strs:
             ans[tuple(sorted(s))].append(s)
         return list(ans.values())
-        
