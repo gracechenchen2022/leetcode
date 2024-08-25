2da886ed40da2826540df2d148dd79741dd9baad
gracechenchen2022 <112357023+gracechenchen2022@users.noreply.github.com>
2024-08-24 20:49:00 -0400

    Runtime: 374 ms (42.34%), Memory: 16.9 MB (60.36%) - Gitcode

--- 774-minimize-max-distance-to-gas-station/774. Minimize Max Distance to Gas Station.py
+++ 774-minimize-max-distance-to-gas-station/774. Minimize Max Distance to Gas Station.py
@@ -2,19 +2,16 @@ class Solution:
     def minmaxGasDist(self, stations: List[int], k: int) -> float:
         left = 0.0
         right = 10**8
-        E = 1e-6  # 浮点精度
+        E = 0.1**6
 
-        while right - left > E:
-            mid = (left + right) / 2
+        while left < right - E:
+            mid = (left + right)/2
             cnt = 0
             for i in range(1, len(stations)):
-                # 计算在当前mid下需要增加的加油站数量
-                cnt += int((stations[i] - stations[i - 1]) / mid)
-            
-            # 根据计算出的加油站数量更新左右边界
+                cnt += int((stations[i] - stations[i-1])/mid)
             if cnt <= k:
                 right = mid
             else:
                 left = mid
-
         return left
+                     
