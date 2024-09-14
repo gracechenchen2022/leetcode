c003e8aafd76e7c02e9fc7aa39620f734e7a120f
gracechenchen2022 <112357023+gracechenchen2022@users.noreply.github.com>
2024-09-14 17:49:43 -0400

    Runtime: 36 ms (50.61%), Memory: 16.5 MB (89.98%) - Gitcode

--- 20-valid-parentheses/20. Valid Parentheses.py
+++ 20-valid-parentheses/20. Valid Parentheses.py
@@ -1,22 +1,12 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # 字典用于存储每种右括号对应的左括号
-        bracket_map = {')': '(', '}': '{', ']': '['}
-        # 栈用于存储遇到的左括号
-        stack = []
-
-        # 遍历字符串中的每个字符
+        bracket_map = {'(': ')', '[': ']', '{': '}'}
+        stack = [] 
         for char in s:
             if char in bracket_map:
-                # 如果当前字符是右括号，从栈中弹出一个元素，如果栈为空则使用一个哑值
-                top_element = stack.pop() if stack else '#'
-                # 如果当前右括号的对应左括号与栈顶元素不匹配，返回 False
-                if bracket_map[char] != top_element:
-                    return False
+                stack.append(bracket_map[char])
             else:
-                # 如果当前字符是左括号，压入栈中
-                stack.append(char)
-
-        # 如果栈为空，说明所有的左括号都被正确匹配
+                if not stack or stack.pop() != char:
+                    return False
+        
         return not stack
-
