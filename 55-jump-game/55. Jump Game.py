28f82ce67352890b7fd92221b025a3e4babcb775
gracechenchen2022 <112357023+gracechenchen2022@users.noreply.github.com>
2024-09-15 17:00:00 -0400

    Runtime: 371 ms (35.67%), Memory: 17.9 MB (35.29%) - Gitcode

--- 55-jump-game/55. Jump Game.py
+++ 55-jump-game/55. Jump Game.py
@@ -1,19 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        # 当前能够到达的最远位置
         max_reachable = 0
-        
-        # 遍历数组中的每个位置
-        for i in range(len(nums)):
-            # 如果当前位置超过了当前能够到达的最远位置，直接返回False
+        for i in range (len(nums)):
             if i > max_reachable:
                 return False
-            
-            # 更新能够到达的最远位置
             max_reachable = max(max_reachable, i + nums[i])
-            
-            # 如果能够到达或超过最后一个位置，直接返回True
-            if max_reachable >= len(nums) - 1:
+            if max_reachable >= len(nums) -1:
                 return True
-        
         return False
