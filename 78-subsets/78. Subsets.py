def99b4dbc49741675995105ea6535a03ec32dba
gracechenchen2022 <112357023+gracechenchen2022@users.noreply.github.com>
2024-09-23 22:38:54 -0400

    Runtime: 35 ms (72.26%), Memory: 16.9 MB (25.26%) - Gitcode

--- 78-subsets/78. Subsets.py
+++ 78-subsets/78. Subsets.py
@@ -10,3 +10,9 @@ class Solution:
         path = []
         backtrack (0, path)
         return res
+
+
+    #tc:o(n*2^n)
+    #总子集数量 2^n 这是因为每个元素有两种选择要么出现在子集中 要么不出现
+    #构建每个子集的时间 复制path到res 需要o(n)
+    #sc:o(n) 递归栈决定n次的空间 路径变量 最大长度n
