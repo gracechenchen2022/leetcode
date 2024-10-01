9ef9e44e1b5d176c8a3060d272a434774f43ce8d
gracechenchen2022 <112357023+gracechenchen2022@users.noreply.github.com>
2024-10-01 00:29:29 -0400

    Runtime: 515 ms (97.39%), Memory: 78.3 MB (39.48%) - Gitcode

--- 146-lru-cache/146. LRU Cache.py
+++ 146-lru-cache/146. LRU Cache.py
@@ -1,62 +1,27 @@
-class DLinkedNode():
-    def __init__(self):
-        self.key = 0 
-        self.value = 0
-        self.prev = None
-        self.next = None
+from collections import OrderedDict
 
 class LRUCache:
-    def _remove_node(self,node):
-        prev = node.prev
-        new = node.next
-        prev.next = new
-        new.prev = prev
-    def _add_node(self, node):
-        node.prev = self.head
-        node.next = self.head.next
-        self.head.next.prev = node
-        self.head.next = node
-    def _pop_tail(self):
-        res = self.tail.prev
-        self._remove_node(res)
-        return res
 
     def __init__(self, capacity: int):
-        self.cache = {}
-        self.size = 0
+        self.cache = OrderedDict()
         self.capacity = capacity
-        self.head, self.tail = DLinkedNode(), DLinkedNode()
-        self.head.next = self.tail
-        self.tail.prev = self.head
+
     def get(self, key: int) -> int:
-        node = self.cache.get(key, None)
-        if not node:
+        if key not in self.cache:
             return -1
-        self._remove_node(node)
-        self._add_node(node)
-        return node.value
+        else:
+            # 移动到末尾表示最近使用
+            self.cache.move_to_end(key)
+            return self.cache[key]
 
     def put(self, key: int, value: int) -> None:
-        node = self.cache.get(key)
-        if not node:
-            newNode = DLinkedNode()
-            newNode.key = key
-            newNode.value = value
-            self.cache[key] = newNode
-            self._add_node(newNode)
-            self.size += 1
-            if self.size > self.capacity:
-                tail = self._pop_tail()
-                del self.cache[tail.key]
-                self.size -= 1
-        else:
-            node.value = value
-            self._remove_node(node)
-            self._add_node(node)
-
-
-        
-
+        if key in self.cache:
+            # 更新值并移到末尾
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            # 弹出第一个元素（最久未使用）
+            self.cache.popitem(last=False)
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
