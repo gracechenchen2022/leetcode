60df6dae0bfe00525a222379f8f1ce9d27bd194c
gracechenchen2022 <112357023+gracechenchen2022@users.noreply.github.com>
2024-10-02 19:07:40 -0400

    Runtime: 10 ms (6.75%), Memory: 54.6 MB (5.10%) - Gitcode

---
+++ 680-valid-palindrome-ii/680. Valid Palindrome II.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while(i<j && s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+        }
+            if(i >= j) return true;
+            if(isPalin(s,i+1,j)||isPalin(s,i,j-1))return true;
+            return false;
+    }
+    private boolean isPalin(String s, int i, int j){
+        while(i<j){
+            if(s.charAt(i) == s.charAt(j)){//avoid deee
+                i++;
+                j--;
+            }else return false;
+        }
+        return true;
+    }
+
+}
+
+// abca
+// aba
+// abc
