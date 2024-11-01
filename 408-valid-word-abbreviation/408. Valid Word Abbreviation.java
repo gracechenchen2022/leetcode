e82acc63821895b36bdaf91306cc30ffec5d97fe
gracechenchen2022 <112357023+gracechenchen2022@users.noreply.github.com>
2024-10-31 21:56:20 -0400

    Runtime: 2 ms (14.63%), Memory: 42.9 MB (8.07%) - Gitcode

--- 408-valid-word-abbreviation/408. Valid Word Abbreviation.java
+++ 408-valid-word-abbreviation/408. Valid Word Abbreviation.java
@@ -8,12 +8,11 @@ class Solution {
                 i++;
                 j++;
                 continue;
-            }//小于等于0的就不要
+            }
             if(abbr.charAt(j) <= '0' || abbr.charAt(j) > '9'){
                 return false;
             }
             int start = j;
-            //在(0,9]之间的数字 就继续走从第一个数字start开始算
             while(j < abbr.length() && abbr.charAt(j) >= '0' && abbr.charAt(j) <='9'){
                 ++j;
             }
