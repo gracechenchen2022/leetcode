7efa27998238d64adba47cdfff410a98fce884b4
gracechenchen2022 <112357023+gracechenchen2022@users.noreply.github.com>
2024-11-13 20:39:41 -0500

    Runtime: 69 ms (59.28%), Memory: 20.9 MB (25.87%) - Gitcode

--- 227-basic-calculator-ii/227. Basic Calculator II.py
+++ 227-basic-calculator-ii/227. Basic Calculator II.py
@@ -1,13 +1,13 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        s = s.replace(' ','')
         stack = []
         num = 0
         sign = '+'
-        s = s.replace(' ','')
-        for i, ch in enumerate(s):
-            if ch.isdigit():
-                num = num * 10 + int(ch)
-            if ch in '+-*/' or i == len(s)-1:
+        for i, char in enumerate(s):
+            if char.isdigit():
+                num = num * 10 + int(char)
+            if char in "+-*/" or i == len(s) - 1:
                 if sign == '+':
                     stack.append(num)
                 elif sign == '-':
@@ -16,6 +16,6 @@ class Solution:
                     stack.append(stack.pop()*num)
                 elif sign == '/':
                     stack.append(int(stack.pop()/num))
-                sign = ch
+                sign = char
                 num = 0
         return sum(stack)
